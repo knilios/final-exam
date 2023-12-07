9e110c97cc7096e037b48d6ae16e4999a4e6b54c
Kristawan Jaroenrad <kristawanja@gmail.com>
2023-12-07 10:32:39 +0700

    Working code but no readability

--- polygon_art.py
+++ polygon_art.py
@@ -9,7 +9,6 @@ class Drawer:
         turtle.colormode(255)
 
     def __random_state(self):
-        self.__num_sides = random.randint(3, 5) # triangle, square, or pentagon
         self.__size = random.randint(50, 150)
         self.__orientation = random.randint(0, 90)
         self.__location = [random.randint(-300, 300), random.randint(-200, 200)]
@@ -40,7 +39,8 @@ class Drawer:
     def draw_complex_polygon(self, num_sides, reduction_ratio = 0.618):
         self.__random_state()
         self.__get_new_color()
-        for _ in range(3):
+        self.__draw_polygon_base_mothod(num_sides)
+        for _ in range(2):
             # reposition the turtle and get a new location
             turtle.penup()
             turtle.forward(self.__size*(1-reduction_ratio)/2)
@@ -55,59 +55,77 @@ class Drawer:
 
             # draw the second polygon embedded inside the original 
             self.__draw_polygon_base_mothod(num_sides)
+        turtle.penup()
 
-drawer = Drawer()
-drawer.draw_complex_polygon(3)
+class Options:
+    def __init__(self):
+        self.__drawer = Drawer()
 
-# def draw_polygon(num_sides, size, orientation, location, color, border_size):
-#     """
-#     This thing draws a polygon, only one
-#     """
-#     turtle.penup()
-#     turtle.goto(location[0], location[1])
-#     turtle.setheading(orientation)
-#     turtle.color(color)
-#     turtle.pensize(border_size)
-#     turtle.pendown()
-#     for _ in range(num_sides):
-#         turtle.forward(size)
-#         turtle.left(360/num_sides)
-#     turtle.penup()
-# 
-# def get_new_color():
-#     return (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-# 
-# turtle.speed(0)
-# turtle.bgcolor('black')
-# turtle.tracer(0)
-# turtle.colormode(255)
-# 
-# # draw a polygon at a random location, orientation, color, and border line thickness
-# num_sides = random.randint(3, 5) # triangle, square, or pentagon
-# size = random.randint(50, 150)
-# orientation = random.randint(0, 90)
-# location = [random.randint(-300, 300), random.randint(-200, 200)]
-# color = get_new_color()
-# border_size = random.randint(1, 10)
-# draw_polygon(num_sides, size, orientation, location, color, border_size)
-# 
-# # specify a reduction ratio to draw a smaller polygon inside the one above
-# reduction_ratio = 0.618
-# 
-# # reposition the turtle and get a new location
-# turtle.penup()
-# turtle.forward(size*(1-reduction_ratio)/2)
-# turtle.left(90)
-# turtle.forward(size*(1-reduction_ratio)/2)
-# turtle.right(90)
-# location[0] = turtle.pos()[0]
-# location[1] = turtle.pos()[1]
-# 
-# # adjust the size according to the reduction ratio
-# size *= reduction_ratio
-# 
-# # draw the second polygon embedded inside the original 
-# draw_polygon(num_sides, size, orientation, location, color, border_size)
-# 
-# # hold the window; close it by clicking the window close 'x' mark
+    def __option_1(self, num):
+        for _ in range(num):
+            self.__drawer.draw_simple_polygon(3)
+
+    def __option_2(self, num):
+        for _ in range(num):
+            self.__drawer.draw_simple_polygon(4)
+
+    def __option_3(self, num):
+        for _ in range(num):
+            self.__drawer.draw_simple_polygon(5)
+    
+    def __option_4(self, num):
+        for _ in range(num):
+            num_sides = random.randint(3, 5) # triangle, square, or pentagon
+            self.__drawer.draw_simple_polygon(num_sides)
+
+    def __option_5(self, num):
+        for _ in range(num):
+            self.__drawer.draw_complex_polygon(3)
+
+    def __option_6(self, num):
+        for _ in range(num):
+            self.__drawer.draw_complex_polygon(4)
+
+    def __option_7(self, num):
+        for _ in range(num):
+            self.__drawer.draw_complex_polygon(5)
+    
+    def __option_8(self, num):
+        for _ in range(num):
+            num_sides = random.randint(3, 5) # triangle, square, or pentagon
+            self.__drawer.draw_complex_polygon(num_sides)
+
+    def choose(self, option, num = 20):
+        if num == "":
+            num = 20
+        num_int = int(num)
+        if num_int <= 0:
+            raise ValueError("Numbers of polygon must be bigger than 0")
+        if option == 1:
+            self.__option_1(num_int)
+        elif option == 2:
+            self.__option_2(num_int)
+        elif option == 3:
+            self.__option_3(num_int)
+        elif option == 4:
+            self.__option_4(num_int)
+        elif option == 5:
+            self.__option_5(num_int)
+        elif option == 6:
+            self.__option_6(num_int)
+        elif option == 7:
+            self.__option_7(num_int)
+        elif option == 8:
+            self.__option_8(num_int)
+        else:
+            raise ValueError("Options only contain 1-8")
+
+
+
+_input = int(input("Which art do you want to generate? Enter a number between 1 to 8, inclusive: "))
+
+num = random.randint(20, 50) # In case you want to specified the number of polygons your self, you can do it here.
+options = Options()
+options.choose(_input, num)
+
 turtle.done()
